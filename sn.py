bc1e2a950610c79b354cc15c6cdba6e14a55ee80
AthaRenata <atharenata1@gmail.com>
2024-12-19 15:39:45 +0700

    first commit

--- sn.py
+++ sn.py
@@ -2,4 +2,4 @@ n = int(input("Masukkan nilai n : "))
 total = 0
 for i in range(1,n+1):
     total += i
-print(f"Jumlah bilangan dari 1 hingga ${n} adalah ${total}")
+print(f"Jumlah bilangan dari 1 hingga {n} adalah {total}")
